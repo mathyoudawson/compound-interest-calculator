1decc46807cfafd93d5a0211f0ecaafa1f84f9cc
Matthew Dawson <matthewdawson@outlook.com>
2020-02-14 19:03:36 +1300

    Remove references to compound interests

--- app/helpers/compound_interests_helper.rb
+++
@@ -1,2 +0,0 @@
-module CompoundInterestsHelper
-end
--- app/models/compound_interest.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class CompoundInterest < ApplicationRecord
-  def calculate_compound_interest
-    self.amount = principal * (1 + interest_rate) ** years
-  end
-end
